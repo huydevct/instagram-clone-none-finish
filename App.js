4e05e3683e7739e5410966839b417437c75fa32c
huydev <64426508+huydevct@users.noreply.github.com>
2021-08-06 00:11:57 +0700

    save data using firebase and redux

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 import firebase from "firebase";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const firebaseConfig = {
   apiKey: "AIzaSyCBup448q3lV6sBfB4DrQCLgh2bfXuCNkI",
   authDomain: "instagram-dev-7b406.firebaseapp.com",
@@ -22,6 +29,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
+import MainScreen from "./components/Main";
 
 const Stack = createStackNavigator();
 export class App extends Component {
@@ -75,10 +83,10 @@ export class App extends Component {
     }
 
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>User is logged in</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <MainScreen />
+      </Provider>
+    )
   }
 }
 
